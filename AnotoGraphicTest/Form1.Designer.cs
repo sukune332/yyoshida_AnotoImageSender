cbf5ed8c214d13757dfa54d4ef2391006ea5379a
Suzuki Tomomichi <sukune332@gmail.com>
2013-11-18 14:07:07 +0900

    initial commit

---
+++ AnotoGraphicTest/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace AnotoGraphicTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 94);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(895, 584);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(788, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "XML読み込み";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(832, 47);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "画像読込";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(832, 76);
+            this.button3.Name = "button3";
+            this.button3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "画像保存";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(832, 116);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "メール送信";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 685);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ AnotoGraphicTest/Form1.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace AnotoGraphicTest
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap bmp,imgbmp;
+        Brush br;
+        Pen p;
+        int x1 = 20;
+        int y1 = 20;
+        int x = 500;
+        int y = 500;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            g = this.CreateGraphics();
+            bmp = new Bitmap(1, 1);
+            bmp.SetPixel(0, 0, Color.Black);
+            br = new SolidBrush(Color.Blue);
+            p = new Pen(br,5);
+        }
+
+        void drawText()
+        {
+            for (int i = x1; i < x; i += x1)
+            {
+                for (int j = y1; j < y; j += y1)
+                {
+                    g.DrawImageUnscaled(bmp, i, j);
+                }
+            }
+
+            double[] xx = new double[]{
+                09.875,
+                10.375,
+                10.375,
+                11.125,
+                14.875,
+                17.25,
+                16.75,
+                16.875,
+                17.375,
+                17.625,
+                18.25,
+                18.125,
+                18.125,
+
+                08.375,
+                08.5,
+                10.125,
+                13.625,
+                23.375,
+                26.0,
+                25.625
+
+
+            };
+            double[] yy = new double[]{
+                10.625,
+                10.375,
+                10.5,
+                09.875,
+                05.125,
+                00.875,
+                04.875,
+                13.75,
+                16.675,
+                28.375,
+                34.25,
+                33.875,
+                33.0,
+
+                34.875,
+                34.875,
+                35.0,
+                34.875,
+                32.875,
+                33.0,
+                32.875
+            };
+
+            // List版
+            List<double> listtmpx = new List<double>();
+            List<List<double>> listx = new List<List<double>>();
+            List<double> listtmpy = new List<double>();
+            List<List<double>> listy = new List<List<double>>();
+
+            listtmpx.Add(09.875);
+            listtmpx.Add(10.375);
+            listtmpx.Add(10.375);
+            listtmpx.Add(11.125);
+            listtmpx.Add(14.875);
+            listtmpx.Add(17.25);
+            listtmpx.Add(16.75);
+            listtmpx.Add(16.875);
+            listtmpx.Add(17.375);
+            listtmpx.Add(17.625);
+            listtmpx.Add(18.25);
+            listtmpx.Add(18.125);
+            listtmpx.Add(18.125);
+            listx.Add(listtmpx);
+            
+            // もう一回初期化
+            listtmpx = new List<double>();
+            listtmpx.Add(08.375);
+            listtmpx.Add(08.5);
+            listtmpx.Add(10.125);
+            listtmpx.Add(13.625);
+            listtmpx.Add(23.375);
+            listtmpx.Add(26.0);
+            listtmpx.Add(25.625);
+            listx.Add(listtmpx);
+
+            listtmpy.Add(10.625);
+            listtmpy.Add(10.375);
+            listtmpy.Add(10.5);
+            listtmpy.Add(09.875);
+            listtmpy.Add(05.125);
+            listtmpy.Add(00.875);
+            listtmpy.Add(04.875);
+            listtmpy.Add(13.75);
+            listtmpy.Add(16.675);
+            listtmpy.Add(28.375);
+            listtmpy.Add(34.25);
+            listtmpy.Add(33.875);
+            listtmpy.Add(33.0);
+            listy.Add(listtmpy);
+
+            listtmpy = new List<double>();
+            listtmpy.Add(34.875);
+            listtmpy.Add(34.875);
+            listtmpy.Add(35.0);
+            listtmpy.Add(34.875);
+            listtmpy.Add(32.875);
+            listtmpy.Add(33.0);
+            listtmpy.Add(32.875);
+            listy.Add(listtmpy);
+
+
+            /*
+            for (int i = 0; i < xx.Length-1; i++)
+            {
+                //e.Graphics.DrawImageUnscaled(b, (int)xx[i]*8, (int)yy[i]*8);
+                //e.Graphics.FillRectangle(br, (int)xx[i]*8, (int)yy[i]*8, 5, 5);
+                e.Graphics.DrawLine(p, (int)xx[i] * 8, (int)yy[i] * 8, (int)xx[i + 1] * 8, (int)yy[i+1] * 8);
+            }
+             */
+
+            for (int i = 0; i < listy.Count; i++)
+            {
+                for (int j = 0; j < listy[i].Count - 1; j++)
+                {
+                    g.DrawLine(p, (int)listx[i][j] * 8, (int)listy[i][j] * 8, (int)listx[i][j + 1] * 8, (int)listy[i][j + 1] * 8);
+                }
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // 動的に書くものなくなっちゃった。
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定する
+            //はじめに「ファイル名」で表示される文字列を指定する
+            //ofd.FileName = "default.html";
+            //はじめに表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            //ofd.InitialDirectory = @"C:\";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter =
+                "アノトストロークファイル(*.xml)|*.xml|すべてのファイル(*.*)|*.*";
+            //[ファイルの種類]ではじめに
+            //「すべてのファイル」が選択されているようにする
+            //ofd.FilterIndex = 2;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            //ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            //ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            //ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき
+                XmlSerialize(ofd.FileName);
+            }
+        }
+
+
+        public void XmlSerialize(String path)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(path);
+
+            for (int i = 0; i < ds.Tables["sp"].Rows.Count; i++)
+            {
+                Console.WriteLine("sp["+i+"] x:"+ds.Tables["sp"].Rows[i][2]+"y:"+ds.Tables["sp"].Rows[i][3]);
+            }
+
+            // 1画の座標数
+            for (int i = 0; i < ds.Tables["stroke"].Rows.Count; i++)
+            {
+                Console.WriteLine("stroke[" + i + "]:" + ds.Tables["stroke"].Rows[i][0]);
+            }
+
+            
+            List<List<double>> listx = new List<List<double>>();
+            List<List<double>> listy = new List<List<double>>();
+            int strokeCount = 0;
+
+            for (int i = 0; i < ds.Tables["stroke"].Rows.Count; i++)
+            {
+                List<double> listtmpx = new List<double>();
+                List<double> listtmpy = new List<double>();
+
+                for (int j = 0; j < int.Parse(ds.Tables["stroke"].Rows[i][0].ToString()); j++)
+                {
+                    listtmpx.Add(double.Parse(ds.Tables["sp"].Rows[strokeCount][2].ToString()));
+                    listtmpy.Add(double.Parse(ds.Tables["sp"].Rows[strokeCount][3].ToString())-800);
+                    Console.WriteLine("x:" + listtmpx[j]+" y:"+listtmpy[j]);
+                    strokeCount++;
+                }
+                listx.Add(listtmpx);
+                listy.Add(listtmpy);
+            }
+
+            for (int i = 0; i < listy.Count; i++)
+            {
+                for (int j = 0; j < listy[i].Count - 1; j++)
+                {
+                    g.DrawLine(p, (int)listx[i][j]*4 , (int)listy[i][j]*4 , (int)listx[i][j + 1]*4 , (int)listy[i][j + 1]*4 );
+                }
+            }
+
+
+
+            /*
+            XmlReader xmlReader = XmlReader.Create(path);
+
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType == XmlNodeType.Element)
+                {
+                    switch (xmlReader.LocalName)
+                    {
+                        case "sample":
+                            System.Console.WriteLine("type" + xmlReader.ReadString());
+                            break;
+                        case "name":
+                            System.Console.WriteLine("name" + xmlReader.ReadString());
+                            break;
+                        case "locality":
+                            System.Console.WriteLine("locality" + xmlReader.ReadString());
+                            break;
+                        case "weight":
+                            System.Console.WriteLine("weight" + xmlReader.ReadString());
+                            break;
+                    }
+                }
+            }
+            */
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //はじめのファイル名を指定する
+            //はじめに「ファイル名」で表示される文字列を指定する
+            //ofd.FileName = "default.html";
+            //はじめに表示されるフォルダを指定する
+            //指定しない（空の文字列）の時は、現在のディレクトリが表示される
+            //ofd.InitialDirectory = @"C:\";
+            //[ファイルの種類]に表示される選択肢を指定する
+            //指定しないとすべてのファイルが表示される
+            ofd.Filter =
+                "アノトストロークファイル(*.xml)|*.xml|すべてのファイル(*.*)|*.*";
+            //[ファイルの種類]ではじめに
+            //「すべてのファイル」が選択されているようにする
+            //ofd.FilterIndex = 2;
+            //タイトルを設定する
+            ofd.Title = "開くファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            //ofd.RestoreDirectory = true;
+            //存在しないファイルの名前が指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            //ofd.CheckFileExists = true;
+            //存在しないパスが指定されたとき警告を表示する
+            //デフォルトでTrueなので指定する必要はない
+            //ofd.CheckPathExists = true;
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                imgbmp = new Bitmap(ofd.FileName);  // ファイルパスから格納
+                pictureBox1.Image = imgbmp; // ピクチャーボックスに表示
+                g = Graphics.FromImage(imgbmp);
+                drawText();
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            //ダイアログを表示する
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき
+                //選択されたファイル名を表示する
+                //Console.WriteLine(sfd.FileName);
+
+                System.IO.FileStream fs = (System.IO.FileStream)sfd.OpenFile();
+      
+         this.pictureBox1.Image.Save(fs, System.Drawing.Imaging.ImageFormat.Jpeg);
+         fs.Close();
+
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //MailMessageの作成
+            System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage();
+            //送信者
+            msg.From = new System.Net.Mail.MailAddress("sender@xxx.xxx");
+            //宛先
+            msg.To.Add(new System.Net.Mail.MailAddress("recipient@xxx.xxx"));
+            //.NET Framework 3.5以前では、以下のようにする
+            //msg.ReplyTo = new System.Net.Mail.MailAddress("replyto@xxx.xxx");
+            //Sender
+            msg.Sender = new System.Net.Mail.MailAddress("master@xxx.xxx");
+
+            //件名
+            msg.Subject = "こんにちは";
+            //本文
+            msg.Body = "こんにちは。\r\n\r\nそれではまた。";
+
+            //メールの配達が遅れたとき、失敗したとき、正常に配達されたときに通知する
+            msg.DeliveryNotificationOptions =
+                System.Net.Mail.DeliveryNotificationOptions.Delay |
+                System.Net.Mail.DeliveryNotificationOptions.OnFailure |
+                System.Net.Mail.DeliveryNotificationOptions.OnSuccess;
+
+            //"C:\test\1.gif"を添付する
+            System.Net.Mail.Attachment attach1 =
+                new System.Net.Mail.Attachment("C:\\test\\1.gif");
+            msg.Attachments.Add(attach1);
+
+            System.Net.Mail.SmtpClient sc = new System.Net.Mail.SmtpClient();
+            //SMTPサーバーなどを設定する
+            sc.Host = "localhost";
+            sc.Port = 25;
+            sc.Host = "smtp.gmail.com";
+            sc.Port = 587;
+            //GMail認証
+            sc.Credentials = new System.Net.NetworkCredential("ID", "pass");
+            //SSL
+            sc.EnableSsl = true;
+            sc.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            //メッセージを送信する
+            sc.Send(msg);
+
+            //後始末
+            msg.Dispose();
+            //後始末（.NET Framework 4.0以降）
+            sc.Dispose();
+        }
+        
+    }
+}
